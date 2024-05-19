ed01f33e8f7e87dbb200cdf9f0c0df5e33830ffa
shantanusinghyo <shantanusinghyo@gmail.com>
2024-05-19 17:37:30 +0530

    basic changes

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  template: '<div><p>This website uses cookies for better experience . </p></div>',
+  styles: ['div{color:brown}']
+})
+export class NotificationComponent {
+
+}
